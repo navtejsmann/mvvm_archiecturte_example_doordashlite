ead05ae9db840a69ea0d55c7fed2026c7effeb9c
Navtej Mann <navtej.maan@gmail.com>
2025-11-11 16:17:02 -0800

    Update gradle version

--- app/src/main/java/com/navtej/doordashlite/di/AppModule.java
+++ app/src/main/java/com/navtej/doordashlite/di/AppModule.java
@@ -1,7 +1,6 @@
 package com.navtej.doordashlite.di;
 
-import android.util.Log;
-
+import com.navtej.doordashlite.BuildConfig;
 import com.navtej.doordashlite.model.DoorDashService;
 import com.navtej.doordashlite.viewmodel.RestaurantViewModelFactory;
 
@@ -10,6 +9,8 @@ import javax.inject.Singleton;
 import androidx.lifecycle.ViewModelProvider;
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -18,11 +19,17 @@ class AppModule {
     @Singleton @Provides
     DoorDashService provideDoorDashService() {
 
-        DoorDashService doorDashService = new Retrofit.Builder()
+        Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl(DoorDashService.HTTPS_API_DOORDASH_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(DoorDashService.class);
+                .addConverterFactory(GsonConverterFactory.create());
+
+        if(BuildConfig.DEBUG) {
+            HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC);
+            OkHttpClient.Builder okHttpBuilder = new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor);
+            builder.client(okHttpBuilder.build());
+        }
+
+        DoorDashService doorDashService = builder.build().create(DoorDashService.class);
 
         return doorDashService;
     }
